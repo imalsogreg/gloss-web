a3b45fcc79950ba9618c4302a485918b7ee4712a
Chris Smith <cdsmith@gmail.com>
2011-08-25 21:51:15 -0600

    Remove gd dependency; re-implement png conversion This may be broken, but so was the old stuff, and this at least removes the rather difficult libgd from the build.

--- src/Instances.hs
+++ src/Instances.hs
@@ -6,12 +6,18 @@
 
 module Instances where
 
+import Blaze.ByteString.Builder
+import Codec.Compression.Zlib
 import Control.Monad
 import Crypto.Hash.MD5
-import Data.Aeson
+import Data.Aeson hiding (Array)
+import Data.Array
+import Data.Bits
 import Data.Ix
+import Data.List
+import Data.Monoid
 import Data.Typeable
-import Graphics.GD hiding (Point)
+import Data.Word
 import Graphics.Gloss
 import System.IO.Unsafe
 import System.Directory
@@ -29,33 +35,72 @@ import qualified Data.ByteString.Base64 as B64
 
 deriving instance Typeable Picture
 
+crcTable :: Array Word8 Word32
+crcTable = array (0,255) [ (i, crcFor i) | i <- [0..255] ]
+    where crcFor n = foldl' step (fromIntegral n) [0..7]
+          step c _ | odd c     = 0xedb88320 `xor` (c `shiftR` 1)
+                   | otherwise = c `shiftR` 1
 
-bmpToPng :: Int -> Int -> ByteString -> IO ByteString
-bmpToPng w h bs = withImage (newImage (w,h)) $ \img -> do
-    forM (range ((0,0), (w-1,h-1))) $ \(x,y) -> do
-        let i         = w * y + x
-        let [r,g,b,a] = map fromIntegral $ B.unpack $ B.take 4 $ B.drop i bs
-        let c         = rgba r g b (a `div` 2)
-        setPixel (x,y) c img
-    savePngByteString img
+crc :: LB.ByteString -> Word32
+crc x = LB.foldl' step 0xffffffff x `xor` 0xffffffff
+  where step c x = (crcTable ! (fromIntegral c `xor` x)) `xor` (c `shiftR` 8)
 
 
-cachedBmpToPng :: Int -> Int -> ByteString -> IO ByteString
+chunk :: ByteString -> LB.ByteString -> Builder
+chunk nm dt = mconcat [
+    fromWord32be (fromIntegral (LB.length dt)),
+    fromByteString nm,
+    fromLazyByteString dt,
+    fromWord32be (crc dt)
+    ]
+
+ifilter :: Int -> ByteString -> LB.ByteString
+ifilter w bs = LB.fromChunks (flist bs)
+  where
+    flist x | B.null x  = []
+            | otherwise = let (s, r) = B.splitAt (4*w) x
+                          in  B.singleton 0 : s : flist r
+
+
+bmpToPng :: Int -> Int -> ByteString -> Builder
+bmpToPng w h bs = mconcat [
+    signature,
+    ihdrChunk,
+    idatChunk,
+    iendChunk
+    ]
+  where
+    signature = fromByteString (B.pack [ 137, 80, 78, 71, 13, 10, 26, 10 ])
+    ihdrChunk = chunk "IHDR" $ toLazyByteString $ mconcat [
+        fromWord32be (fromIntegral w),
+        fromWord32be (fromIntegral h),
+        fromWord8 8,
+        fromWord8 6,
+        fromWord8 0,
+        fromWord8 0,
+        fromWord8 0
+        ]
+    idatChunk = chunk "IDAT" (compress (ifilter w bs))
+    iendChunk = chunk "IEND" LB.empty
+
+
+cachedBmpToPng :: Int -> Int -> ByteString -> IO LB.ByteString
 cachedBmpToPng w h bs = do
     let digest = BC.unpack $ B64.encode $ hash bs
     let fn     = "tmp/" ++ digest ++ ".png"
     e <- doesFileExist fn
     case e of
-        True  -> B.readFile fn
-        False -> do res <- bmpToPng w h bs
-                    B.writeFile fn res
+        True  -> LB.readFile fn
+        False -> do let res = toLazyByteString (bmpToPng w h bs)
+                    LB.writeFile fn res
                     return res
 
 
 toBitmap :: Int -> Int -> ByteString -> Text
 toBitmap w h bs = unsafePerformIO $ do
     png <- cachedBmpToPng w h bs
-    return ("data:image/png;base64," `T.append` T.decodeUtf8 (B64.encode png))
+    let spng = B.concat (LB.toChunks png)
+    return ("data:image/png;base64," `T.append` T.decodeUtf8 (B64.encode spng))
 
 
 instance ToJSON Picture where
