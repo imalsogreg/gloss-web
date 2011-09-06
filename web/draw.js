90e922dd33ee1c058f29768af730a5ef0f36bc4b
Jens Petersen <juhpetersen@gmail.com>
2011-09-06 16:01:51 +0900

    fix animations for circles with negative radius

--- web/draw.js
+++ web/draw.js
@@ -358,7 +358,7 @@ function display(ctx, p, a, b, c, d, e, f)
         ctx.save();
         ctx.transform(a, b, c, d, e, f);
         ctx.beginPath();
-        ctx.arc(0, 0, r, 0, 2 * Math.PI);
+        if (r > 0) ctx.arc(0, 0, r, 0, 2 * Math.PI);
         ctx.restore();
         ctx.stroke();
     }
@@ -371,7 +371,7 @@ function display(ctx, p, a, b, c, d, e, f)
         ctx.transform(a, b, c, d, e, f);
         if (w > 0) ctx.lineWidth = w;
         ctx.beginPath();
-        ctx.arc(0, 0, r, 0, 2 * Math.PI);
+        if (r > 0) ctx.arc(0, 0, r, 0, 2 * Math.PI);
         if (w > 0)
         {
             ctx.stroke();
